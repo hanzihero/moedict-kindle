3baa748cffca4f470de74c38a11ac0c07278ac48
Kevin Lang <me@gnalck.com>
2023-10-14 15:27:09 +0800

    more progress

---
+++ README.md
@@ -0,0 +1,5 @@
+https://github.com/verenablaschke/kindle-dict
+
+https://kdp.amazon.com/en_US/help/topic/G2HXJS944GL88DNV#html
+
+https://jakemccrary.com/blog/2020/11/11/creating-a-custom-kindle-dictionary/
--- moedict.exs
+++ moedict.exs
@@ -1,3 +1,9 @@
+kindlegen_path = "/Applications/Kindle Previewer 3.app/Contents/lib/fc/bin//kindlegen"
+
+if !File.exists?(kindlegen_path) do
+  raise("kindlegen not found at #{kindlegen_path}")
+end
+
 entries = [
   %{
     headword: "你",
@@ -7,3 +13,6 @@ entries = [
 
 html = EEx.eval_file("moedict.html.eex", entries: entries)
 File.write!("moedict.html", html)
+
+IO.puts("hi")
+System.cmd(kindlegen_path, ["-c0", "-verbose", "moeadict.opf"])
